d95395cc579bc51d3f58e45bb74c249f6616bd5a
Drake53 <49623303+Drake53@users.noreply.github.com>
2019-10-08 21:17:30 +0200

    Example package for testing.

---
+++ src/War3Lib.Extensions.Common/TimerExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using War3Net.CodeAnalysis.Common;
+
+using static War3Api.Common;
+
+namespace War3Lib.Extensions.Common
+{
+    [NativeLuaMemberContainer]
+    public static class TimerExtensions
+    {
+        [NativeLuaMember("TimerStart")]
+        public static extern void Start(this timer whichTimer, float timeout, bool periodic, Action handlerFunc);
+    }
+}
